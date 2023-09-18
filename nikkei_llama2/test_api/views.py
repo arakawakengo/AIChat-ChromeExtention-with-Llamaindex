f9b147af272531a23c2da37b205951a72631c5b0
arakawakengo <70435005+arakawakengo@users.noreply.github.com>
2023-09-18 16:23:40 +0900

    add TEST django app

---
+++ nikkei_llama2/test_api/views.py
@@ -0,0 +1,23 @@
+from rest_framework.views import APIView
+from django.http import HttpResponse
+from rest_framework.response import Response
+from django.shortcuts import render
+
+import json
+import typing as T
+
+import logging
+
+class TEST(APIView):
+    
+    def post(self, request):
+        logging.debug(request.data)
+        
+        text = request.data["text"]
+        
+        result = {
+            "text": text + "!",
+            "result": "OK",
+        }
+        
+        return Response(result), 200
