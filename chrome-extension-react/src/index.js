fade9df5a1214b417690ea40dd5ba2201cfbbd00
arakawakengo <70435005+arakawakengo@users.noreply.github.com>
2023-09-19 16:53:37 +0900

    不要箇所の削除

--- chrome-extension-react/src/index.js
+++ chrome-extension-react/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './content.js';
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
