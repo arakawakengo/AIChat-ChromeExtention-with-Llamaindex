34f9cabac65def83ef7c6b607e092a959b13dfca
arakawakengo <70435005+arakawakengo@users.noreply.github.com>
2023-09-19 15:05:03 +0900

    拡張機能で、ウィジェット形式でチャットを表示するようにする

--- chrome-extension-react/src/content.js
+++ chrome-extension-react/src/content.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function MyComponent() {
-  return <div>Hello, world!</div>;
-}
+import ChatWidget from './ChatWidget';
 
 const root = document.createElement('div');
 document.body.appendChild(root);
 
-ReactDOM.render(<MyComponent />, root);
+ReactDOM.render(<ChatWidget />, root);
--- chrome-extension-react/src/index.js
+++ chrome-extension-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
