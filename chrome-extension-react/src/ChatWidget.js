cdf92bf995b118a005124f3a1d606479947b7c4c
arakawakengo <70435005+arakawakengo@users.noreply.github.com>
2023-09-20 14:13:42 +0900

    最低限のExtentionのUIの作成

--- chrome-extension-react/src/ChatWidget.js
+++ chrome-extension-react/src/ChatWidget.js
@@ -7,12 +7,15 @@ import {
     MessageList,
     Message,
     MessageInput,
+    Avatar,
+    ConversationHeader,
 } from "@chatscope/chat-ui-kit-react";
 
 const ChatWidget = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState("");
+    const denshibaIcon = "https://sdl-stickershop.line.naver.jp/products/0/0/1/1101563/android/stickers/4159670.png";
 
     const sendMessage = async () => {
         const userMessage = { message: newMessage, sender: 'You' };
@@ -27,14 +30,20 @@ const ChatWidget = () => {
         setMessages([...messages, userMessage, botMessage]);
     };
 
+    const getDirection = (sender) => {
+        return sender === 'You' ? 'outgoing' : 'incoming';
+    };
   
     return (
-        <div style={{ position: 'fixed', bottom: '10px', right: '10px' }}>
+        <div style={{ position: 'fixed', bottom: '10px', right: '10px', width: "300px" }}>
         <button onClick={() => setIsOpen(!isOpen)}>Toggle Chat</button>
         {isOpen && (
-            <div style={{ border: '1px solid black', padding: '10px' }}>
-            <MainContainer>
-                <ChatContainer>
+            <div style={{ border: '1px solid black', padding: '10px', width: "100%", height: "500px" }}>
+            <ChatContainer>
+                <ConversationHeader>
+                    <Avatar src={denshibaIcon} name="デンシバ" />
+                    <ConversationHeader.Content userName="デンシバ" info="なんでも質問してね！" />       
+                </ConversationHeader>
                 <MessageList>
                     {messages.map((m, index) => (
                     <Message
@@ -43,19 +52,21 @@ const ChatWidget = () => {
                         message: m.message,
                         sentTime: 'just now',
                         sender: m.sender,
+                        direction: getDirection(m.sender)
                         }}
-                    />
+                    >
+                    </Message>
+
                     ))}
                 </MessageList>
-                <MessageInput 
-                    placeholder="Type message here" 
-                    value={newMessage}
-                    onChange={(e) => setNewMessage(e)}
-                    onSend={() => sendMessage()}
-                    attachButton={false} 
-                />
-                </ChatContainer>
-            </MainContainer>
+                    <MessageInput 
+                        placeholder="Type message here" 
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e)}
+                        onSend={() => sendMessage()}
+                        attachButton={false} 
+                    />
+            </ChatContainer>
             </div>
         )}
         </div>
