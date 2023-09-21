defdb347342bfcec41da3cbc3ca4054720dc9d66
arakawakengo <70435005+arakawakengo@users.noreply.github.com>
2023-09-22 05:25:53 +0900

    ウィジェットサイズの修正

--- chrome-extension-react/src/ChatWidget.js
+++ chrome-extension-react/src/ChatWidget.js
@@ -46,40 +46,38 @@ const ChatWidget = () => {
     }, []);
   
     return (
-        <div style={{ position: 'fixed', bottom: '10px', right: '10px', width: "300px" }}>
-        <button onClick={() => setIsOpen(!isOpen)}>Toggle Chat</button>
-        {isOpen && (
-            <div style={{ border: '1px solid black', padding: '10px', width: "100%", height: "500px" }}>
-            <ChatContainer>
-                <ConversationHeader>
-                    <Avatar src={denshibaIcon} name="デンシバ" />
-                    <ConversationHeader.Content userName="デンシバ" info="なんでも質問してね！" />       
-                </ConversationHeader>
-                <MessageList>
-                    {messages.map((m, index) => (
-                    <Message
-                        key={index}
-                        model={{
-                        message: m.message,
-                        sentTime: 'just now',
-                        sender: m.sender,
-                        direction: getDirection(m.sender)
-                        }}
-                    >
-                    </Message>
-
-                    ))}
-                </MessageList>
-                    <MessageInput 
-                        placeholder="Type message here" 
-                        value={newMessage}
-                        onChange={(e) => setNewMessage(e)}
-                        onSend={() => sendMessage()}
-                        attachButton={false} 
-                    />
-            </ChatContainer>
-            </div>
-        )}
+        <div style={{ position: 'fixed', bottom: '10px', right: '10px'}}>
+            <button onClick={() => setIsOpen(!isOpen)} style={{ backgroundColor: '#4CAF50', color: 'white', border: 'none', padding: '10px 20px', borderRadius: '12px', cursor: 'pointer', marginBottom: '10px' }}>Toggle Chat</button>
+            {isOpen && (
+                <div style={{ border: '1px solid black', padding: '10px', width: "30vw", height: "70vh" }}>
+                    <ChatContainer>
+                        <ConversationHeader>
+                            <Avatar src={denshibaIcon} name="デンシバ" />
+                            <ConversationHeader.Content userName="デンシバ" info="記事についてわからないことを聞いてほしいワン！" />       
+                        </ConversationHeader>
+                        <MessageList>
+                            {messages.map((m, index) => (
+                            <Message
+                                key={index}
+                                model={{
+                                message: m.message,
+                                sentTime: 'just now',
+                                sender: m.sender,
+                                direction: getDirection(m.sender)
+                                }}
+                            />
+                            ))}
+                        </MessageList>
+                        <MessageInput 
+                            placeholder="Type message here" 
+                            value={newMessage}
+                            onChange={(e) => setNewMessage(e)}
+                            onSend={() => sendMessage()}
+                            attachButton={false} 
+                        />
+                    </ChatContainer>
+                </div>
+            )}
         </div>
     );
 };
